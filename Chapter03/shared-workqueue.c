bcf29b785eddf85526866b76a227754a9651ec74
Oscar Megia <megia.oscar@gmail.com>
2022-09-25 09:43:57 +0200

    This kfree hangs my vm kernel.
    I needed to change where kfree(my_data) to avoid this hang.
    Now works fine.

--- Chapter03/shared-workqueue.c
+++ Chapter03/shared-workqueue.c
@@ -23,7 +23,6 @@ static void work_handler(struct work_struct *work)
     pr_info("Work queue module handler: %s, data is %d\n", __FUNCTION__, my_data->the_data);
     msleep(3000);
     wake_up_interruptible(&my_data->my_wq);
-    kfree(my_data);
 }
 
 static int __init my_init(void)
@@ -40,6 +39,7 @@ static int __init my_init(void)
     pr_info("I'm goint to sleep ...\n");
     wait_event_interruptible(my_data->my_wq, sleep != 0);
     pr_info("I am Waked up...\n");
+    kfree(my_data);
     return 0;
 }
 
